1b7e3b4e3476e4043a3be883e92d9e005f2e769a
roberth360 <88516476+roberth360@users.noreply.github.com>
2024-01-23 11:23:26 -0600

    Create problema3.py

---
+++ problema3.py
@@ -0,0 +1,15 @@
+
+n = int(input("Ingrese un número: "))
+
+if n % 2 == 0:
+    print(n, "es par.")
+else:
+    print(n, "es impar.")
+	
+	
+4. 
+
+def suma_lista(lista_numeros):
+    return sum(lista_numeros)
+
+print(suma_lista([1, 2, 3, 4, 5]))
