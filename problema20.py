7b3490ed067fefd44ac03020132734f4e73d3f74
roberth360 <88516476+roberth360@users.noreply.github.com>
2024-01-23 11:38:44 -0600

    Create problema20.py
    20.	Define una clase llamada "Calculadora" con métodos estáticos para sumar, restar, multiplicar y dividir dos números.

---
+++ problema20.py
@@ -0,0 +1,17 @@
+class Calculadora:
+
+    @staticmethod
+    def sumar(a, b):
+        return a + b
+
+    @staticmethod
+    def restar(a, b):
+        return a - b
+
+    @staticmethod
+    def multiplicar(a, b):
+        return a * b
+
+    @staticmethod
+    def dividir(a, b):
+        return a / b
